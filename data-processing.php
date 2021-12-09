16db9b16b1d448a30f2d9289782882b74445470e
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-10 00:41:41 +0100

    formulaire -> dans un txt + LECTURE

--- data-processing.php
+++ data-processing.php
@@ -3,8 +3,8 @@ if (!empty($_POST)) {
     $action = $_POST['action'];
     $id = $_POST['identifiant'];
     $email = $_POST['email'];
+    $num = $_POST['numtel'];
     $password = $_POST['password'];
-    $connexionReussie = false;
     if ($action == 'mailer') {
         $file= 'data.txt';
         if(!($file = fopen($file, 'a+'))) {
@@ -19,6 +19,19 @@ if (!empty($_POST)) {
         $query = "SELECT * FROM users WHERE Identifiant = '".$_POST['identifiant']."'";
         $queryResult = mysqli_query($dbLink, $query);
         var_dump($queryResult);
+        if($queryResult == false){
+            $query1 = 'INSERT INTO user (Identifiant, mail, Num_tel) VALUES (\'' . $id . '\', \''
+                . $email . '\',\'' . $email . '\')';
+
+            if (!($dbResult = mysqli_query($dbLink, $query))) {
+                echo 'Erreur dans requête<br />';
+                // Affiche le type d'erreur.
+                echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+                // Affiche la requête envoyée.
+                echo 'Requête : ' . $query . '<br/>';
+                exit();
+            }
+        }
 
     } else {
         echo '<br/><strong>Bouton non géré !</strong><br/>';
