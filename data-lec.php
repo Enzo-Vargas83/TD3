0588d14dfc9be878c42a20efc951aece0944c7c9
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-10 00:08:28 +0100

    formulaire -> dans un txt + LECTURE

--- data-lec.php
+++ data-lec.php
@@ -1,2 +1,16 @@
 <?php
+
+    $file = 'data.txt';
+    if (!($file = fopen($file, 'r'))) {
+        echo 'Erreur de lecture';
+        exit();
+    }
+
+    echo 'Liste des utilisateurs : <br/>';
+    $cpt = 1;
+    while ($line = fgets($file, 255)) {
+         echo 'Utilisateur n ' . $cpt . ' : ' . $line . '<br/>';
+         ++$cpt;
+     }
+
 ?>
