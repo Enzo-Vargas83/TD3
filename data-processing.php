f86c147a01015dd0ab16315b0d7b1561c1cffd92
Enzo-Vargas83 <enzo.vargas@etu.univ-amu.fr>
2021-12-10 01:25:27 +0100

    formulaire -> dans un txt + LECTURE

--- data-processing.php
+++ data-processing.php
@@ -17,12 +17,12 @@ if (!empty($_POST)) {
         <?php
         $dbLink = mysqli_connect('mysql-vargas.alwaysdata.net', 'vargas', 'lolo83520', 'vargas_td2');
 
-        $query = "SELECT * FROM user WHERE mail = '".$_POST['email'].'' and ''.$_POST['password']."'";
+        $query = "SELECT * FROM user WHERE mail = ''".$_POST['email']." AND mdp = ".$_POST['password'].'"";
         $queryResult = mysqli_query($dbLink, $query);
         var_dump($queryResult);
         if(mysqli_num_rows($queryResult) == 0){
-            $query1 = 'INSERT INTO user (mail, Num_tel, mdp) VALUES (\'' . $email . '\', \''
-                . $num . '\', \'' . $password . '\')';
+            $query1 = 'INSERT INTO user (mail, Num_tel) VALUES (\'' . $email . '\', \''
+                . $num . '\',\'' . $password . '\' )';
 
             if (!($dbResult = mysqli_query($dbLink, $query1))) {
                 echo 'Erreur dans requête<br />';
